ab0cf87c775558114f7b8e44b7797e533563721d
Baptiste <baptiste.jardin@etu.univ-grenoble-alpes.fr>
2023-03-07 15:36:11 +0100

    gps_location

--- app/src/main/java/com/example/polymapi/MainActivity.java
+++ app/src/main/java/com/example/polymapi/MainActivity.java
@@ -1,12 +1,43 @@
 package com.example.polymapi;
 
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AlertDialog;
+import androidx.databinding.library.baseAdapters.BuildConfig;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.Settings;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.example.polymapi.databinding.ActivityMainBinding;
+import com.google.android.gms.location.CurrentLocationRequest;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.Granularity;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.Priority;
+import com.google.android.gms.tasks.CancellationTokenSource;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,26 +46,124 @@ public class MainActivity extends AppCompatActivity {
     private boolean tourRunning = false;
     private boolean uploadRunning = false;
 
+
+    ActivityMainBinding binding;
+    private FusedLocationProviderClient fusedLocationClient;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
 
-        // Get reference to the buttons
+/*        // Get reference to the buttons
         tourButton = findViewById(R.id.tour);
         uploadButton = findViewById(R.id.upload);
 
         // Set the initial text of the button
         tourButton.setOnClickListener(view -> toggleTourMode());
 
-        uploadButton.setOnClickListener(view -> toggleUploadMode());
+        uploadButton.setOnClickListener(view -> toggleUploadMode());*/
 
+        binding= ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+
+        binding.tour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (hasLocationPermissions()) {
+                    getLastLocation();
+                }else{
+                    if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
+                        showCustomDialog("Location Permission", "This app needs the location permission to track your location", "Ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int which) {
+                                multiplePermissionLauncher.launch(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION});
+                            }
+                        }, "cancel", null);
+                    }else {
+                        multiplePermissionLauncher.launch(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION});
+                    }
+                }
+            }
+        });
     }
 
-    /**
+    @SuppressLint({"Missing permission", "MissingPermission"})
+    private void getLastLocation(){
+        CurrentLocationRequest currentLocationRequest = new CurrentLocationRequest.Builder()
+                .setGranularity(Granularity.GRANULARITY_FINE)
+                .setPriority(Priority.PRIORITY_HIGH_ACCURACY)
+                .setDurationMillis(5000)
+                .setMaxUpdateAgeMillis(0)
+                .build();
+
+        CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+
+        fusedLocationClient.getCurrentLocation(currentLocationRequest, cancellationTokenSource.getToken()).addOnCompleteListener(new OnCompleteListener<Location>() {
+            @Override
+            public void onComplete(@NonNull Task<Location> task) {
+                if(task.isSuccessful()){
+                    Location location = task.getResult();
+                    Log.d("test", "on complete: " + location);
+                } else {
+                    task.getException().printStackTrace();
+                }
+            }
+        });
+    }
+
+
+    private boolean hasLocationPermissions(){
+        return checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    void showCustomDialog(String title, String message,
+                          String positiveBtnTitle, DialogInterface.OnClickListener positiveListener,
+                          String negativeBtnTitle, DialogInterface.OnClickListener negativeListener) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(title)
+                .setMessage(message)
+                .setPositiveButton(positiveBtnTitle, positiveListener)
+                .setNegativeButton(negativeBtnTitle, negativeListener);
+        builder.create().show();
+    }
+
+    private ActivityResultLauncher<String[]> multiplePermissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(), new ActivityResultCallback<Map<String, Boolean>>() {
+        @Override
+        public void onActivityResult(Map<String, Boolean> result) {
+            boolean finePermissionAllowed = false;
+            if(result.get(Manifest.permission.ACCESS_FINE_LOCATION) != null) {
+                finePermissionAllowed = result.get(Manifest.permission.ACCESS_FINE_LOCATION);
+                if(finePermissionAllowed) {
+                    getLastLocation();
+                }else {
+                    if(!shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)){
+                        showCustomDialog("Location Permission", "Need fine location permission, allow it in the app settings", "GoTo Settings", new DialogInterface.OnClickListener(){
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int which) {
+                                Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                                        Uri.parse("package" + BuildConfig.LIBRARY_PACKAGE_NAME));
+                                startActivity(intent);
+                            }
+                        }, "cancel", null);
+                    }
+                }
+            }
+        }
+    });
+
+
+
+
+/*    *//**
      * Updates the text of the Hello button.
      *
-     */
+     *//*
     private void toggleTourMode() {
         if(tourRunning && uploadRunning) {
             throw new RuntimeException("Should never happen");
@@ -81,5 +210,6 @@ public class MainActivity extends AppCompatActivity {
         // Add the row to the table layout
         myLayout.addView(row);
     }
+    */
 
 }

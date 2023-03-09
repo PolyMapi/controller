ff5e2acb7e5921ef90d7acb3850db8380a590c77
gregiorg <gergely.fodor@etu.univ-grenoble-alpes.fr>
2023-03-09 14:26:15 +0100

    cleanup in Main

--- app/src/main/java/com/example/polymapi/MainActivity.java
+++ app/src/main/java/com/example/polymapi/MainActivity.java
@@ -224,35 +224,4 @@ public class MainActivity extends AppCompatActivity {
         myLayout.addView(row);
     }
 
-    private void databaseTest() {
-        // Gets the data repository in write mode
-        SQLiteDatabase db = dbHelper.getWritableDatabase();
-
-        // Clear the table by deleting all rows
-        int rowsDeleted = db.delete(FeedReaderContract.ImgRefsEntry.TABLE_NAME, null, null);
-
-        // Create a new map of values, where column names are the keys
-        ContentValues values = new ContentValues();
-        values.put(FeedReaderContract.ImgRefsEntry.COLUMN_NAME_CAPTURE_ID, 7);
-        values.put(FeedReaderContract.ImgRefsEntry.COLUMN_NAME_REF, "1234567");
-
-        // Insert the new row, returning the primary key value of the new row
-        long newRowId = db.insert(FeedReaderContract.ImgRefsEntry.TABLE_NAME, null, values);
-
-        Cursor cursor = db.query(
-                FeedReaderContract.ImgRefsEntry.TABLE_NAME,   // The table to query
-                null,                                         // The array of columns to return (pass null to get all)
-                null,                                         // The columns for the WHERE clause
-                null,                                         // The values for the WHERE clause
-                null,                                         // don't group the rows
-                null,                                         // don't filter by row groups
-                null                                          // The sort order
-        );
-
-        cursor.moveToNext();
-        int capture_Id = cursor.getInt(cursor.getColumnIndexOrThrow(FeedReaderContract.ImgRefsEntry.COLUMN_NAME_CAPTURE_ID));
-        cursor.close();
-
-    }
-
 }

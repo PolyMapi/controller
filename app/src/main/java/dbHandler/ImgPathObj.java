1c19401dde8e5f8fa682921021b6b52e2f01bc04
gregiorg <gergely.fodor@etu.univ-grenoble-alpes.fr>
2023-03-09 11:17:45 +0100

    added struct like classes for the database

---
+++ app/src/main/java/dbHandler/ImgPathObj.java
@@ -0,0 +1,11 @@
+package dbHandler;
+
+public class ImgPathObj {
+    public int captureId;
+    public String imgPath;
+
+    public ImgPathObj(int captureId, String imgPath) {
+        this.captureId = captureId;
+        this.imgPath = imgPath;
+    }
+}
---
+++ app/src/main/java/dbHandler/ImgRefObj.java
@@ -0,0 +1,11 @@
+package dbHandler;
+
+public class ImgRefObj {
+    public int captureId;
+    public String imgRef;
+
+    public ImgRefObj(int captureId, String imgRef) {
+        this.captureId = captureId;
+        this.imgRef = imgRef;
+    }
+}

929086654531dcc0d99a127b0ed4c05a2f2e6d88
gregiorg <gergely.fodor@etu.univ-grenoble-alpes.fr>
2023-03-16 16:27:15 +0100

    Coordinates problem fixed

--- app/src/main/java/exif/ExifHandler.java
+++ app/src/main/java/exif/ExifHandler.java
@@ -41,6 +41,7 @@ public class ExifHandler {
     public static void writeLongitude(String fileName, MainActivity mainActivity, double value) throws IOException {
         ExifInterface exifInterface = checkWritingPermission(fileName, mainActivity);
         String dmsLongitude = decimalToDms(value);
+        Log.d("Debug", "writeLongitude: dms : " + dmsLongitude);
         exifInterface.setAttribute(ExifInterface.TAG_GPS_LONGITUDE, dmsLongitude);
         exifInterface.saveAttributes();
     }
@@ -48,6 +49,7 @@ public class ExifHandler {
     public static void writeLatitude(String fileName, MainActivity mainActivity, double value) throws IOException {
         ExifInterface exifInterface = checkWritingPermission(fileName, mainActivity);
         String dmsLatitude = decimalToDms(value);
+        Log.d("Debug", "writeLatitude: dms : " + dmsLatitude);
         exifInterface.setAttribute(ExifInterface.TAG_GPS_LATITUDE, dmsLatitude);
         exifInterface.saveAttributes();
     }
@@ -82,11 +84,18 @@ public class ExifHandler {
     }
 
     private static String decimalToDms(double decimalDegrees) {
-        int degrees = (int) decimalDegrees;
+        /*int degrees = (int) decimalDegrees;
         decimalDegrees = Math.abs(decimalDegrees - degrees) * 60;
         int minutes = (int) decimalDegrees;
         decimalDegrees = (decimalDegrees - minutes) * 60;
-        int seconds = (int) (decimalDegrees * 1000);
-        return degrees + "/1," + minutes + "/1," + seconds + "/1000";
+        int seconds = (int) (decimalDegrees * 3600);
+        return degrees + "/1," + minutes + "/1," + seconds + "/1000";*/
+
+        int d = (int)decimalDegrees;
+        double tmp = (decimalDegrees - d) * 60;
+        int m = (int) tmp;
+        double s = (tmp - m) * 60;
+
+        return d + "/1," + m + "/1," + s + "/1000";
     }
 }
